4985fae00f1b08b058334a68a8861075c66f0504
ʟᴇᴏ ᠰ ᴛɢ <ilashadam039@gmail.com>
2024-09-07 22:00:13 +0400

    Update info.py

--- info.py
+++ info.py
@@ -16,12 +16,12 @@ API_ID = int(os.environ.get('API_ID', ''))
 API_HASH = os.environ.get('API_HASH', '')
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 PORT = os.environ.get("PORT", "8080")
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '')]
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7040444713 2144812475')]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002438161993'))
 
 # for mongodb
-DATABASE_NAME = os.environ.get("DB_NAME", "")     
-DATABASE_URI  = os.environ.get("DB_URL", "")
+DATABASE_NAME = os.environ.get("DB_NAME", "auto")     
+DATABASE_URI  = os.environ.get("DB_URL", "mongodb+srv://hi:hi@auto.natzh.mongodb.net/?retryWrites=true&w=majority&appName=auto")
 MONGO_URL = os.environ.get('MONGO_URL', "")
 
 
